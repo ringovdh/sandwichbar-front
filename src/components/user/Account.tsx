3c1cbb9e5789f4a6e5ac7109bb64c9753cfc20cf
Ringo Van den Hende <ringo.vandenhende@faros.be>
2024-06-03 10:01:11 +0200

    fix accountform

--- src/components/user/Account.tsx
+++ src/components/user/Account.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useRef} from "react";
+import React, {useContext, useEffect, useRef, useState} from "react";
 import Button from "../ui/button/Button";
 import "./Account.css";
 import authenticationService from "../../services/AuthenticationService";
@@ -6,13 +6,18 @@ import UpdateUserRequest from "../../entities/request/updateUserRequest";
 import UserContext from "../../store/UserContext";
 
 const Account = () => {
-
     const userCtx = useContext(UserContext);
+    const [username, setUsername] = useState<string>(userCtx.userName)
     const usernameTextInputRef = useRef<HTMLInputElement>(null);
 
+    useEffect(() => {
+        if (usernameTextInputRef.current) {
+            usernameTextInputRef.current.focus()
+        }
+    }, [])
     function handleSubmit(event: React.FormEvent) {
         event.preventDefault();
-        const updateUserRequest = new UpdateUserRequest(userCtx.userName);
+        const updateUserRequest = new UpdateUserRequest(username);
 
         authenticationService.updateUser(updateUserRequest)
     }
@@ -25,20 +30,24 @@ const Account = () => {
                     <label htmlFor="user-email">Email</label>
                     <input type="email"
                            id="user-email"
-                           value={userCtx.userEmail} disabled={true}/>
+                           value={userCtx.userEmail}
+                           disabled={true}/>
                 </p>
                 <p className="control">
-                        <label htmlFor="user-name">User name</label>
-                        <input type="text" id="user-name" value={userCtx.userName}/>
-                    </p>
+                    <label htmlFor="user-name">User name *</label>
+                    <input type="text"
+                           id="user-name"
+                           value={username}
+                           onChange={event => setUsername(event.target.value)}
+                           required/>
+                </p>
 
-                    <p className="account-actions">
-                        <Button
-                            textOnly={false}
+                <p className="account-actions">
+                    <Button textOnly={false}
                             className="">
-                            Save
-                        </Button>
-                    </p>
+                        Save
+                    </Button>
+                </p>
             </form>
         </div>
     );
